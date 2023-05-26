63607cabfdbbf10b0be07e3b2f08fcdc43d5f0a2
Paulo Ricardo <paulodev07@gmail.com>
2023-05-26 10:39:39 -0300

    Desenvolvimento dos components e ajustes na Home

--- src/hook/useForm.js
+++ src/hook/useForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export const useForm = (initialForm = {}) => {
-  const [form, setform] = useState(initialForm);
+  const [formState, setFormState] = useState(initialForm);
   const onInputChange = ({ target }) => {
     const { name, value } = target;
 
